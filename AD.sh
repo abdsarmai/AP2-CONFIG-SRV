f04709407a8f78e72c4d47e5b02023f6392a5eae
Abdallah Lisser BTS SIO LLA <144156904+abdallahlisserbtssiolla@users.noreply.github.com>
2024-02-14 19:53:02 +0100

    Update AD.sh

--- AD.sh
+++ AD.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #Mettre à jour la liste des paquets et les mettre à jour 
+
 apt-get update && apt-get upgrade
+#Installer les paquets nécessaires pour la suite de l'AP.
 
 apt install packagekit samba-common-bin sssd-tools sssd libnss-sss libpam-sss policykit-1 sssd ntpdate ntp realmd
+
+#Se connecter au domaine avec l'utilisateur administrateur qui gère le domaine 
 
 realm join --user=admin-1O@CleanergyO.local CleanergyO.local
